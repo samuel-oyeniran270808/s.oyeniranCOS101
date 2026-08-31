4cccdf37122646df6badc49f71695086e7e16dc1
Samuel Oyeniran <310343185+samuel-oyeniran2708@users.noreply.github.com>
2026-08-31 23:51:23 +0100

    (week-2 projects)

---
+++ week-2/project/project_1.rs
@@ -0,0 +1,31 @@
+fn main() {
+	
+	let p:f64 = 520_000_000.00;
+
+	let r:f64 = 10.00;
+
+	
+	let amount:f64 = p * (1.00 + (r / 100.00)) * (1.00 + (r / 100.00)) * (1.00 + (r / 100.00)) * (1.00 + (r / 100.00)) * (1.00 + (r / 100.00));
+
+	let si = amount - p;
+
+	println!("The simple interest is {}", si);
+
+/* Here, i noticed that not using the variable would flag an error, so i decided to do the tradition maths of multipllying the pr 5 times and i undeerstand that this is not convenient and is lackluster. 
+	So i researched on how to use the powers in Rust and i did a separate one just below using it.*/
+
+
+	let principal:f64 = 520_000_000.00;
+
+	let rate:f64 = 10.00;
+
+	let time:i32 = 5;
+
+	let amount_2:f64 = principal * (1.00 + (rate / 100.00)).powi(time);
+	let simple_interest = amount_2 - principal;
+
+	println!("The simple interest is {}", simple_interest);
+
+
+
+}
