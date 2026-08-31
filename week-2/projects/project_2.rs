0560a030d2d1a89e116da16bffe89d2107bf9aea
Samuel Oyeniran <310343185+samuel-oyeniran2708@users.noreply.github.com>
2026-08-31 17:06:08 +0100

    (Week_2 projects)

---
+++ week-2/projects/project_2.rs
@@ -0,0 +1,12 @@
+fn main() {
+	
+	println!(); //prints just a
+	
+	println!("hello "); //prints hello
+	
+	println!("format {} arguement", "some"); //prints format some arguements
+
+	println!("My name is {}. I am the class rep of stream 1", "Samuel Oyeniran");
+
+
+}
---
+++ week-2/projects/project_3.rs
@@ -0,0 +1,7 @@
+fn main() {
+	let fees = 25_000; // 25_000 is a value stored in the variable 'fees'
+
+	let salary: f64 = 35_000.00; // likewise here however, the 'f64' shows that it is a decimal value with a memory of 64bits which should be equal to 2.1 billion and above.
+
+	println!("fees in {} and salary is {}", fees, salary);
+}
